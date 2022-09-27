148c425af8ecf7fe68e624f90c3821bfec702abd
Ali Alhussein <a.alhussein33@gmail.com>
2022-09-28 01:50:15 +0300

    cors

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import bodyParser from 'body-parser';
-
+import cors from 'cors';
 import { dbConn } from "./config/db.config.js";
 
 const app = express();
 const PORT = 5000;
 
+app.use(cors({
+  origin: 'http://localhost:5000'
+}));
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   dbConn;
